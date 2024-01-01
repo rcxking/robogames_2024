10b75646acd9cff5ac509e28062ff8ab0774448b
Bryant Pong <rcxking@gmail.com>
2023-12-31 22:53:42 -0800

    Runner supports Simulation Runs
    This commit passes in a "use_sim" flag to the runner module to denote
    that the run is for a simulation run.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

--- ros_workspace/src/runner/src/runner.cpp
+++ ros_workspace/src/runner/src/runner.cpp
@@ -9,11 +9,10 @@
 #include <coordinate_parser/CoordinateParser.h>
 #include <ros/ros.h>
 
-#include <cstdio>
 #include <string>
 
 int main(int argc, char **argv) {
-	printf("%s:%d: Starting RoboMagellan 2024 Run\n", __FUNCTION__, __LINE__);
+	ROS_INFO("Starting RoboMagellan 2024 Run");
 
 	// ROS initialization
 	ros::init(argc, argv, "runner");
@@ -24,34 +23,34 @@ int main(int argc, char **argv) {
 	// Grab the filepath containing the mission's coordinates
 	std::string filepath;
 	if (nh.getParam("filepath", filepath)) {
-		printf("%s:%d: Will load mission coordinates from filepath: %s\n",
-				__FUNCTION__, __LINE__, filepath.c_str());
+		ROS_INFO("Will load mission coordinates from filepath: %s", filepath.c_str());
 	} else {
-
-		fprintf(stderr, "%s:%d: ERROR: filepath does not exist!\n",
-				__FUNCTION__, __LINE__);
+		ROS_ERROR("Filepath %s does not exist!", filepath.c_str());
 		return 1;
 	}
 
+	// Check if we're using the simulator or not
+	bool use_sim;
+	nh.param("use_sim", use_sim, false);
+
 	// Parse GPS coordinates
 	CoordinateParser cp;
-	if (!cp.LoadCoordinateFile(filepath)) {
-		fprintf(stderr, "%s:%d: ERROR: Unable to parse coordinates from file: %s\n",
-				__FUNCTION__, __LINE__, filepath.c_str());
+	if (!cp.LoadCoordinateFile(filepath, use_sim)) {
+		ROS_ERROR("Unable to parse coordinates from file: %s", filepath.c_str());
 		return 1;
 	}
 
 	// Display mission coordinates and ask for user confirmation before starting
 	cp.DisplayMissionCoordinates();
-	printf("If the coordinates are correct, enter \'Y\' to proceed\n");
+	ROS_INFO("If the coordinates are correct, enter \'Y\' to proceed");
 	char input;
 	scanf("%s", &input);
 	if (input != 'Y') {
 		// Quit
-		printf("Aborting run\n");
+		ROS_INFO("Aborting run");
 		return 0;
 	} else {
-		printf("Starting run\n");
+		ROS_INFO("Starting run");
 	}
 
 	while (ros::ok()) {
