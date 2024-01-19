a0f6603fcd65be297a0d3d895aae86d40e47806d
Bryant Pong <rcxking@gmail.com>
2024-01-18 18:19:22 -0800

    Adding launch file and initial rpi_motors_node

---
+++ ros_workspace/src/rpi_motors/src/rpi_motors_node.py
@@ -0,0 +1,14 @@
+# rpi_motors_node.py
+# Interface node from the Raspberry Pi to the Talon SRX motor controllers.
+# !!! THIS SHOULD ONLY BE RUN ON THE RASPBERRY PI !!!
+#
+# Bryant Pong
+# 1/18/24
+
+import rospy
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
