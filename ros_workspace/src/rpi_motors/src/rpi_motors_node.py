3d36e5ca490b157dfd20e68bb0a8e7a858065220
Bryant Pong <rcxking@gmail.com>
2024-01-19 18:19:53 -0800

    Adding rpi_motors.srv

--- ros_workspace/src/rpi_motors/src/rpi_motors_node.py
+++ ros_workspace/src/rpi_motors/src/rpi_motors_node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env
+
 # rpi_motors_node.py
 # Interface node from the Raspberry Pi to the Talon SRX motor controllers.
 # !!! THIS SHOULD ONLY BE RUN ON THE RASPBERRY PI !!!
@@ -8,7 +10,10 @@
 import rospy
 
 def main():
-    pass
+    rospy.loginfo('Starting Raspberry Pi Motors Node')
+    rospy.init_node('rpi_motors_node')
+    rospy.loginfo('Ready to process and send motor commands')
+    rospy.spin()
 
 if __name__ == '__main__':
     main()
