7dbc5f283bd294d7be1f7c678b5b0f515cfe221b
Bryant Pong <rcxking@gmail.com>
2024-01-23 18:17:33 -0800

    Updating RPIMotors.srv

--- ros_workspace/src/rpi_motors/src/rpi_motors_node.py
+++ ros_workspace/src/rpi_motors/src/rpi_motors_node.py
@@ -10,22 +10,10 @@
 from rpi_motors.srv import RPIMotors
 import rospy
 
-# Last desired motor percentages (-100% - 100%)
-last_left_motor_percentage = 0
-last_right_motor_percentage = 0
-
 # Callback to handle sending motor percentages
 def HandleMotorCommand(req):
-    rospy.loginfo('Received left motor: ' + str(req.left_power_percentage) +
-            '%; right motor:' + str(req.right_power_percentage) + '%')
-    # Ensure requested percentages are within range
-    req_left_percent = max(-100, req.left_power_percentage)
-    req_left_percent = min(100, req_left_percent)
-    req_right_percent = max(-100, req.right_power_percentage)
-    req_right_percent = min(100, req_right_percent)
-
-    rospy.loginfo('Sending left motor: ' + str(req_left_percent) +
-            '%; right motor: ' + str(req_right_percent) + '%')
+    rospy.loginfo('Received left motor: ' + str(req.left_desired_velocity) +
+            'm/s; right motor:' + str(req.right_desired_velocity) + 'm/s')
 
     return True
 
