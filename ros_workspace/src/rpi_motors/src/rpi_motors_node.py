68199e68c41098ea65e0d020db54765605e70743
Bryant Pong <rcxking@gmail.com>
2024-01-25 19:55:00 -0800

    Node ready for testing

--- ros_workspace/src/rpi_motors/src/rpi_motors_node.py
+++ ros_workspace/src/rpi_motors/src/rpi_motors_node.py
@@ -9,7 +9,7 @@
 
 from odometry.msg import Velocities
 from rpi_motors.srv import RPIMotors
-#import pigpio
+import pigpio
 import rospy
 
 # Class to perform the proportional motor controls
@@ -20,43 +20,42 @@ class RPIMotorsControl:
         self._kp_left = rospy.get_param('~kp_left')
         self._kp_right = rospy.get_param('~kp_right')
         rospy.loginfo('kp_left: ' + str(self._kp_left) + '; kp_right: ' + str(self._kp_right))
-        
+
         # Current linear velocities (m/s)
         self._cur_left_vel = 0.0
         self._cur_right_vel = 0.0
-        
+
         # Desired linear velocities (m/s)
         self._des_left_vel = 0.0
         self._des_right_vel = 0.0
-        
+
         # Current motor PWM signals (1000 - 2000).  1500 is STOP; 2000 is full
         # forward; 1000 is full reverse.
         self._cur_left_pwm = 1500
         self._cur_right_pwm = 1500
-        
+
         # GPIO interface.  GPIO 18 is for the left motor; GPIO 13 is for the
         # right motor.
-        #self._pi = pigpio.pi()
+        self._pi = pigpio.pi()
         self._LEFT_GPIO_PIN = 18
         self._RIGHT_GPIO_PIN = 13
-        
+
         # Send the stop motors command (1500)
-        #self._pi.set_servo_pulsewidth(self._LEFT_GPIO_PIN, 1500)
-        #self._pi.set_servo_pulsewidth(self._RIGHT_GPIO_PIN, 1500)
-        
+        self.StopMotors()
+
         # Start Subscriber to the current motor velocities
         rospy.Subscriber('/odometry/current_velocities', Velocities, self.HandleVelocityMsg)
-    
+
         # Start service to send motor commands
         motor_service = rospy.Service('rpi_motor_commands', RPIMotors, self.HandleMotorCommand)
-    
+
         rospy.loginfo('Ready to process and send motor commands')
 
     # Callback to update robot's current linear velocities
     def HandleVelocityMsg(self, msg):
         self._cur_left_vel = msg.left_velocity
         self._cur_right_vel = msg.right_velocity
-        
+
     # Callback to update receiving desired motor velocities
     def HandleMotorCommand(self, req):
         rospy.loginfo('Received left motor: ' + str(req.left_desired_velocity) +
@@ -64,8 +63,13 @@ class RPIMotorsControl:
         self._des_left_vel = req.left_desired_velocity
         self._des_right_vel = req.right_desired_velocity
         return True
-    
-    # Callback to compute the next motor command
+
+    # Helper to stop both motors
+    def StopMotors(self):
+        self._pi.set_servo_pulsewidth(self._LEFT_GPIO_PIN, 1500)
+        self._pi.set_servo_pulsewidth(self._RIGHT_GPIO_PIN, 1500)
+
+    # Compute the next motor commands
     def ComputeMotorCommand(self):
         # Compute the error between the desired and current velocities (des - cur).
         # If the difference is positive, the current velocity is less than the
@@ -75,38 +79,38 @@ class RPIMotorsControl:
         # need to be made.
         left_error = self._des_left_vel - self._cur_left_vel
         right_error = self._des_right_vel - self._cur_right_vel
-        rospy.loginfo('des_left_vel: ' + str(self._des_left_vel) + '; cur_left_vel: ' + 
+        rospy.loginfo('des_left_vel: ' + str(self._des_left_vel) + '; cur_left_vel: ' +
                       str(self._cur_left_vel) + '; des_right_vel: ' + str(self._des_right_vel) +
-                      '; cur_right_vel: ' + str(self._cur_right_vel) + '; left_error: ' + 
+                      '; cur_right_vel: ' + str(self._cur_right_vel) + '; left_error: ' +
                       str(left_error) + '; right_error: ' + str(right_error))
-        
+
         # Apply proportional constants to determine the change in velocity (m/s)
         delta_left_vel_ms = self._kp_left * left_error
         delta_right_vel_ms = self._kp_right * right_error
         rospy.loginfo('delta_left_vel_ms: ' + str(delta_left_vel_ms) + '; delta_right_vel_ms: ' + str(delta_right_vel_ms))
-        
+
         # Convert velocities from m/s to motor commands
         delta_left_vel_pwm = 90.909 * delta_left_vel_ms + 1500
         delta_right_vel_pwm = 90.909 * delta_right_vel_ms + 1500
         rospy.loginfo('delta_left_vel_pwm: ' + str(delta_left_vel_pwm) + '; delta_right_vel_pwm: ' + str(delta_right_vel_pwm))
-        
+
         # Apply current velocity commands
         self._cur_left_pwm += delta_left_vel_pwm
         self._cur_right_pwm += delta_right_vel_pwm
         rospy.loginfo('cur_left_pwm: ' + str(self._cur_left_pwm) + '; cur_right_pwm: ' + str(self._cur_right_pwm))
-        
+
         # Threshold the current left/right pwm command to be in range [1000, 2000]
         self._cur_left_pwm = max(1000, self._cur_left_pwm)
         self._cur_left_pwm = min(2000, self._cur_left_pwm)
-        
+
         self._cur_right_pwm = max(1000, self._cur_right_pwm)
         self._cur_right_pwm = min(2000, self._cur_right_pwm)
         rospy.loginfo('Thresholded cur_left_pwm: ' + str(self._cur_left_pwm) + '; thresholded cur_right_pwm: ' + str(self._cur_right_pwm))
-        
+
         # Send motor commands
-        #self._pi.set_servo_pulsewidth(self._LEFT_GPIO_PIN, self._cur_left_pwm)
-        #self._pi.set_servo_pulsewidth(self._RIGHT_GPIO_PIN, self._cur_right_pwm)
-        
+        self._pi.set_servo_pulsewidth(self._LEFT_GPIO_PIN, self._cur_left_pwm)
+        self._pi.set_servo_pulsewidth(self._RIGHT_GPIO_PIN, self._cur_right_pwm)
+
     # Main loop
     def spin(self):
         # TODO: Make this a rosparam
@@ -114,24 +118,21 @@ class RPIMotorsControl:
         while not rospy.is_shutdown():
             self.ComputeMotorCommand()
             rate.sleep()
-            
+
         # On shutdown, stop the motors and perform cleanup
         rospy.loginfo('Stopping RPI motors node')
-        #self._pi.set_servo_pulsewidth(self._LEFT_GPIO_PIN, 1500)
-        #self._pi.set_servo_pulsewidth(self._RIGHT_GPIO_PIN, 1500)
-        #self._pi.stop()
+        self.StopMotors()
+        self._pi.stop()
 
 def main():
     rospy.loginfo('Starting Raspberry Pi Motors Node')
     rospy.init_node('rpi_motors_node')
-    
+
     # Ensure pigpio daemon is started before sending commands
-    '''
     if not pi.connected:
         rospy.loginfo('ERROR: pigpio daemon not started')
         return
-    '''
-    
+
     # Construct motor controller object
     controller = RPIMotorsControl()
     controller.spin()
