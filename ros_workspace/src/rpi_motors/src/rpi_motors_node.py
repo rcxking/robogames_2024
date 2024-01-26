b81d8c52dcb7a85fa1af1d325ebcb6493527ee66
Bryant Pong <rcxking@gmail.com>
2024-01-25 17:00:24 -0800

    Moved all functions to the class

--- ros_workspace/src/rpi_motors/src/rpi_motors_node.py
+++ ros_workspace/src/rpi_motors/src/rpi_motors_node.py
@@ -39,53 +39,52 @@ class RPIMotorsControl(self):
         self._LEFT_GPIO_PIN = 18
         self._RIGHT_GPIO_PIN = 13
 
-
-# Callback to update current linear velocities
-def HandleVelocityMsg(msg):
-    cur_left_vel = msg.left_velocity
-    cur_right_vel = msg.right_velocity
-
-# Callback to handle sending motor percentages
-def HandleMotorCommand(req):
-    rospy.loginfo('Received left motor: ' + str(req.left_desired_velocity) +
-            'm/s; right motor:' + str(req.right_desired_velocity) + 'm/s')
-    des_left_vel = req.left_desired_velocity
-    des_right_vel = req.right_desired_velocity
-    return True
-
-# Compute next motor command
-def ComputeMotorCommand():
-    # Compute the error between the desired and current velocities (des - cur).
-    # If the difference is positive, the current velocity is less than the
-    # desired velocity and the motor needs to speed up.  If the difference
-    # is negative, the current velocity is greater than the desired velocity and
-    # the motor needs to slow up.  At a difference of 0 no additional changes
-    # need to be made.
-    left_error = des_left_vel - cur_left_vel
-    right_error = des_right_vel - cur_right_vel
+    # Callback to update robot's current linear velocities
+    def HandleVelocityMsg(self, msg):
+        self._cur_left_vel = msg.left_velocity
+        self._cur_right_vel = msg.right_velocity
+        
+    # Callback to update receiving desired motor velocities
+    def HandleMotorCommand(self, req):
+        rospy.loginfo('Received left motor: ' + str(req.left_desired_velocity) +
+                      'm/s; right motor:' + str(req.right_desired_velocity) + 'm/s')
+        des_left_vel = req.left_desired_velocity
+        des_right_vel = req.right_desired_velocity
+        return True
     
-    # Apply proportional constants to determine the change in velocity (m/s)
-    delta_left_vel_ms = kp_left * left_error
-    delta_right_vel_ms = kp_right * right_error
-    
-    # Convert velocities from m/s to motor commands
-    delta_left_vel_pwm = 90.909 * delta_left_vel_ms + 1500
-    delta_right_vel_pwm = 90.909 * delta_right_vel_ms + 1500
-    
-    # Apply current velocity commands
-    cur_left_pwm += delta_left_vel_pwm
-    cur_right_pwm += delta_right_vel_pwm
-    
-    # Threshold the current left/right pwm command to be in range [1000, 2000]
-    cur_left_pwm = max(1000, cur_left_pwm)
-    cur_left_pwm = min(2000, cur_left_pwm)
-    
-    cur_right_pwm = max(1000, cur_right_pwm)
-    cur_right_pwm = min(2000, cur_right_pwm)
-    
-    # Send motor commands
-    #pi.set_servo_pulsewidth(LEFT_GPIO_PIN, cur_left_pwm)
-    #pi.set_servo_pulsewidth(RIGHT_GPIO_PIN, cur_right_pwm)
+    # Callback to compute the next motor command
+    def ComputeMotorCommand():
+        # Compute the error between the desired and current velocities (des - cur).
+        # If the difference is positive, the current velocity is less than the
+        # desired velocity and the motor needs to speed up.  If the difference
+        # is negative, the current velocity is greater than the desired velocity and
+        # the motor needs to slow up.  At a difference of 0 no additional changes
+        # need to be made.
+        left_error = self._des_left_vel - self._cur_left_vel
+        right_error = self._des_right_vel - self._cur_right_vel
+        
+        # Apply proportional constants to determine the change in velocity (m/s)
+        delta_left_vel_ms = self._kp_left * left_error
+        delta_right_vel_ms = self._kp_right * right_error
+        
+        # Convert velocities from m/s to motor commands
+        delta_left_vel_pwm = 90.909 * delta_left_vel_ms + 1500
+        delta_right_vel_pwm = 90.909 * delta_right_vel_ms + 1500
+        
+        # Apply current velocity commands
+        self._cur_left_pwm += delta_left_vel_pwm
+        self._cur_right_pwm += delta_right_vel_pwm
+        
+        # Threshold the current left/right pwm command to be in range [1000, 2000]
+        self._cur_left_pwm = max(1000, self._cur_left_pwm)
+        self._cur_left_pwm = min(2000, self._cur_left_pwm)
+        
+        self._cur_right_pwm = max(1000, self._cur_right_pwm)
+        self._cur_right_pwm = min(2000, self._cur_right_pwm)
+        
+        # Send motor commands
+        #self._pi.set_servo_pulsewidth(self._LEFT_GPIO_PIN, self._cur_left_pwm)
+        #self._pi.set_servo_pulsewidth(self._RIGHT_GPIO_PIN, self._cur_right_pwm)
 
 def main():
     rospy.loginfo('Starting Raspberry Pi Motors Node')
