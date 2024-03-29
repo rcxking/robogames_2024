20ef090909789e1034f62f0cb08887f90f0cfd97
Bryant <rcxking@gmail.com>
2024-03-28 20:07:43 -0700

    IMU Prototyping Code (#41)
    This commit adds in a standalone Arduino sketch to prototype the IMU.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

---
+++ prototyping_code/imu_test/imu_test.ino
@@ -0,0 +1,112 @@
+#include "ICM_20948.h"
+
+#define SERIAL_PORT Serial
+#define WIRE_PORT Wire
+#define AD0_VAL 1
+
+// Using I2C
+ICM_20948_I2C myICM;
+
+void setup() {
+  SERIAL_PORT.begin(115200);
+
+  // Wait for the serial port to be active
+  while (!SERIAL_PORT) {
+  }
+
+  WIRE_PORT.begin();
+  WIRE_PORT.setClock(400000);
+
+  bool initialized = false;
+  while (!initialized) {
+    myICM.begin(WIRE_PORT, AD0_VAL);
+
+    SERIAL_PORT.print(F("Initialization of the sensor returned: "));
+    SERIAL_PORT.println(myICM.statusString());
+    if (myICM.status != ICM_20948_Stat_Ok) {
+      SERIAL_PORT.println("Trying again...");
+      delay(500);
+    } else {
+      initialized = true;
+    }
+  }
+}
+
+void loop() {
+  if (myICM.dataReady()) {
+    // Update the values from the IMU
+    myICM.getAGMT();
+    printScaledAGMT(&myICM);
+    delay(30);
+  } else {
+    // IMU isn't ready
+    SERIAL_PORT.println("Waiting for data");
+    delay(500);
+  }
+}
+
+void printFormattedFloat(float val, uint8_t leading, uint8_t decimals)
+{
+  float aval = abs(val);
+  if (val < 0)
+  {
+    SERIAL_PORT.print("-");
+  }
+  else
+  {
+    SERIAL_PORT.print(" ");
+  }
+  for (uint8_t indi = 0; indi < leading; indi++)
+  {
+    uint32_t tenpow = 0;
+    if (indi < (leading - 1))
+    {
+      tenpow = 1;
+    }
+    for (uint8_t c = 0; c < (leading - 1 - indi); c++)
+    {
+      tenpow *= 10;
+    }
+    if (aval < tenpow)
+    {
+      SERIAL_PORT.print("0");
+    }
+    else
+    {
+      break;
+    }
+  }
+  if (val < 0)
+  {
+    SERIAL_PORT.print(-val, decimals);
+  }
+  else
+  {
+    SERIAL_PORT.print(val, decimals);
+  }
+}
+
+void printScaledAGMT(ICM_20948_I2C *sensor) {
+  SERIAL_PORT.print("Scaled. Acc (mg) [ ");
+  printFormattedFloat(sensor->accX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->accZ(), 5, 2);
+  SERIAL_PORT.print(" ], Gyr (DPS) [ ");
+  printFormattedFloat(sensor->gyrX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->gyrZ(), 5, 2);
+  SERIAL_PORT.print(" ], Mag (uT) [ ");
+  printFormattedFloat(sensor->magX(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magY(), 5, 2);
+  SERIAL_PORT.print(", ");
+  printFormattedFloat(sensor->magZ(), 5, 2);
+  SERIAL_PORT.print(" ], Tmp (C) [ ");
+  printFormattedFloat(sensor->temp(), 5, 2);
+  SERIAL_PORT.print(" ]");
+  SERIAL_PORT.println();
+}
