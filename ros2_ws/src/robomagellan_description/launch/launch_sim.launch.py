e51254bde32c47dd665ccd0cd1f6416d05361c8a
Bryant <rcxking@gmail.com>
2024-12-10 17:37:41 -0800

    Updating Robomagellan URDF and ROS 2 Gazebo Sim Launch (#52)
    This commit corrects the Robomagellan URDF to have the proper TF frames
    such as `base_footprint` and `base_link`.  The inertial matrix for the
    wheels was also fixed.  Finally, a Python launch file is added to launch
    the Gazebo sim, process the URDF file, and spawn the simulated robot.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

---
+++ ros2_ws/src/robomagellan_description/launch/launch_sim.launch.py
@@ -0,0 +1,36 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+import os
+
+def generate_launch_description():
+    # Include robot_state_publisher launch file
+    package_name = 'robomagellan_description'
+
+    rv = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory(package_name), 'launch',
+                'robot_visualization.launch.py'
+            )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # Include Gazebo launch file from package gazebo_ros
+    gazebo = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch',
+                'gazebo.launch.py')])
+    )
+
+    # Spawn an instance of the simulated robot
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+            arguments=['-topic', 'robot_description', '-entity', 'robot'],
+            output='screen')
+
+    return LaunchDescription([
+        rv,
+        gazebo,
+        spawn_entity
+    ])
