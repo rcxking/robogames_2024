27f2b7b0ce2aa743bcc1a8875a51cf9eaa5834d6
Bryant <rcxking@gmail.com>
2026-03-10 17:26:06 -0700

    Initial robomagellan_bringup Package (#55)
    This commit adds in the initial `robomagellan_bringup` package, which
    contains the launch files used for a RoboMagellan competition.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

---
+++ ros2_ws/src/robomagellan_bringup/launch/competition.launch.py
@@ -0,0 +1,11 @@
+'''
+competition.launch.py
+
+Launch file for the real robot to be used during a RoboMagellan competition.
+'''
+from launch import LaunchDescription
+
+# "main" function for launch files
+def generate_launch_description():
+    return LaunchDescription([
+    ])
