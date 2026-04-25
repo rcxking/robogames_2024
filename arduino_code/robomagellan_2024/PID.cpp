6f2e0de0571edaf71a3cf5a5e56df4adb76749f7
Bryant Pong <rcxking@gmail.com>
2026-04-24 20:21:55 -0700

    Implemented PID control

--- arduino_code/robomagellan_2024/PID.cpp
+++ arduino_code/robomagellan_2024/PID.cpp
@@ -5,3 +5,35 @@
  * motor.
  */
 #include "PID.h"
+
+void PID::SendPWMCommand(const double cur_vel, const double des_vel) {
+  /*
+   * Compute the error between the desired and current velocities (des - cur).
+   * If the difference is positive, the current velocity is less than the
+   * desired velocity and the motors needs to speed up.  If the difference is
+   * negative, the current velocity is greater than the desired velocity and the
+   * motor needs to slow down.  At a difference of 0 no additional changes need
+   * to be made.
+   */
+  const double error = des_vel - cur_vel;
+
+  // Add to accumulated errors integral term
+  accumulated_error_ += (ki_ * error);
+
+  // Compute derivative term
+  const double deriv = error - prev_error_;
+
+  // Apply proportional constants to determine the change in velocity (rad/s)
+  const double delta_vel_rad_per_sec = (kp_ * error) + (accumulated_error_) +
+                                       (kd_ * deriv);
+
+  // Update last error
+  prev_error_ = error;
+
+  // Apply current PWM command
+  cur_pwm_cmd_ += delta_vel_rad_per_sec;
+
+  // Threshold curren PWM command to be in range [1000, 2000]
+  cur_pwm_cmd_ = max(1000, cur_pwm_cmd_);
+  cur_pwm_cmd_ = min(2000, cur_pwm_cmd_);
+}
--- arduino_code/robomagellan_2024/PID.h
+++ arduino_code/robomagellan_2024/PID.h
@@ -15,14 +15,38 @@ public:
     kp_(kp), ki_(ki), kd_(kd) {}
 
   /*
+   * Given the current and desired motor velocities compute and send the next
+   * PWM motor command to the motor controller.
+   *
+   * Parameters:
+   *   cur_vel (double): Current wheel velocity (rad/s)
+   *   des_vel (double): Desired wheel velocity (rad/s)
+   */
+  void SendPWMCommand(const double cur_vel, const double des_vel);
+
+private:
+  // Proportional, integral, derivative constants
+  double kp_ = 0.0;
+  double ki_ = 0.0;
+  double kd_ = 0.0;
+
+  // Previous error
+  double prev_error_ = 0.0;
+
+  // Accumulated error for integral term
+  double accumulated_error_ = 0.0;
+
+  /*
+   * Current motor controller PWM signal.  Needs to be in the range [1000,
+   * 2000].  1500 is STOP; 2000 is full forward; and 1000 is full reverse.
+   */
+  int cur_pwm_cmd_ = 1500;
+
+  /*
    * Given the current and desired motor velocities compute the next PWM command
    * to send to the motor controllers.
    */
   int ComputePWMCommand(const double cur_vel, const double des_vel) const;
-
-private:
-  // Proportional, integral, derivative constants
-  double kp_, ki_, kd_;
 };
 
 #endif
