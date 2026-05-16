3a188ace442646ebc8dc81c67417ec15ba6736fa
Bryant <rcxking@gmail.com>
2026-05-15 22:29:52 -0700

    Arduino - Arduino Calculates Wheel Position Deltas (#82)
    This commit updates the Arduino code to correctly compute the wheel
    positions and velocities and publishes these 4 values through the Serial
    console to the ROS stack.
    
    Signed-off-by: Bryant Pong <rcxking@gmail.com>

--- arduino_code/robomagellan_2024/robomagellan_2024.ino
+++ arduino_code/robomagellan_2024/robomagellan_2024.ino
@@ -36,9 +36,9 @@ unsigned long last_time_ms = 0;
 // Time (in ms) to compute the wheel velocities
 const unsigned long COMPUTE_TIME_MS = 100;
 
-// Current left/right wheel velocities (rad/s)
-double left_wheel_vel_rad_per_sec  = 0.0;
-double right_wheel_vel_rad_per_sec = 0.0;
+// Last wheel positions (4x ticks)
+int32_t prev_left_ticks_4x  = 0;
+int32_t prev_right_ticks_4x = 0;
 
 /*
  * CimCoder V2 tick to radians conversion factor.
@@ -55,6 +55,23 @@ double right_wheel_vel_rad_per_sec = 0.0;
  *           10           72-tooth gear    720
  */
 const double TICKS_TO_RADIANS = (11.0 * PI / 720.0);
+
+/*
+ * Compute the difference between the current and previous tick count.
+ * This handles tick rollover by temporarily promoting the tick counts
+ * as int64_t before performing the difference.
+ *
+ * Parameters:
+ *   cur_ticks_4x: Current ticks (4x counting)
+ *   prev_ticks_4x: Previous ticks (4x counting)
+ *
+ * Returns:
+ *   (int32_t): Difference of cur_ticks_4x - prev_ticks_4x
+ */
+int32_t CalculateTicksDifference(const int32_t cur_ticks_4x, const int32_t prev_ticks_4x) {
+  const int64_t diff = cur_ticks_4x - prev_ticks_4x;
+  return static_cast<int32_t>(diff);
+}
 
 void setup() {
   // Wait for a connection to the Raspberry Pi
@@ -79,25 +96,52 @@ void loop() {
   const unsigned long time_delta_ms   = current_time_ms - last_time_ms;
 
   if (time_delta_ms >= COMPUTE_TIME_MS) {
-    // Encoder library uses 4x counting so need to divide read() ticks by 4 to get true tick count
-    const int32_t true_left_ticks  = leftEncoder.read() / 4;
-    const int32_t true_right_ticks = rightEncoder.read() / 4;
+    /*
+     * Encoder library uses 4x counting so need to divide read() ticks by 4 to
+     * get the true tick count. The CimCoders should return 20 ticks per
+     * revolution.
+     */
+    const int32_t cur_left_ticks_4x  = leftEncoder.read();
+    const int32_t cur_right_ticks_4x = rightEncoder.read();
 
-    // Compute/update wheel velocities
-    const double time_delta_s = time_delta_ms / 1000.0;
-    left_wheel_vel_rad_per_sec  = (true_left_ticks * TICKS_TO_RADIANS) / time_delta_s;
-    right_wheel_vel_rad_per_sec = (true_right_ticks * TICKS_TO_RADIANS) / time_delta_s;
+    // Compute 4x count tick deltas
+    const int32_t delta_left_ticks_4x  = CalculateTicksDifference(cur_left_ticks_4x, prev_left_ticks_4x);
+    const int32_t delta_right_ticks_4x = CalculateTicksDifference(cur_right_ticks_4x, prev_right_ticks_4x);
+
+    // Compute the true number of ticks
+    const float true_left_ticks        = cur_left_ticks_4x / 4.0;
+    const float true_right_ticks       = cur_right_ticks_4x / 4.0;
+
+    const float true_delta_left_ticks  = delta_left_ticks_4x / 4.0;
+    const float true_delta_right_ticks = delta_right_ticks_4x / 4.0;
+
+    const double time_delta_s   = time_delta_ms / 1000.0;
+
+    // Compute wheel positions (rad)
+    const double left_wheel_pos_rads  = true_left_ticks * TICKS_TO_RADIANS;
+    const double right_wheel_pos_rads = true_right_ticks * TICKS_TO_RADIANS;
+
+    // Compute wheel velocities (rad/s)
+    const double left_wheel_vel_rad_per_sec  = (true_delta_left_ticks * TICKS_TO_RADIANS) / time_delta_s;
+    const double right_wheel_vel_rad_per_sec = (true_delta_right_ticks * TICKS_TO_RADIANS) / time_delta_s;
 
     // Debugs for wheel velocities
 #if ENABLE_DEBUGS
-    Serial.print("time_delta_s: ");
-    Serial.print(time_delta_s);
-    Serial.print(" TICKS_TO_RADIANS: ");
-    Serial.print(TICKS_TO_RADIANS);
+    Serial.print("cur_left_ticks_4x: ");
+    Serial.print(cur_left_ticks_4x);
+    Serial.print(" cur_right_ticks_4x: ");
+    Serial.print(cur_right_ticks_4x);
+
     Serial.print(" true_left_ticks: ");
     Serial.print(true_left_ticks);
     Serial.print(" true_right_ticks: ");
     Serial.print(true_right_ticks);
+
+    Serial.print(" left_wheel_pos_rads: ");
+    Serial.print(left_wheel_pos_rads);
+    Serial.print(" right_wheel_pos_rads: ");
+    Serial.print(right_wheel_pos_rads);
+
     Serial.print(" left_wheel_vel_rad_per_sec: ");
     Serial.print(left_wheel_vel_rad_per_sec);
     Serial.print(" right_wheel_vel_rad_per_sec: ");
@@ -107,19 +151,23 @@ void loop() {
     /*
      * Publish wheel velocities to the Raspberry Pi.  The data is a string of the format:
      *
-     * L<left wheel velocity in rad/sec>;R<right wheel velocity in rad/sec>;
+     * L<left wheel position (rad)>,<left wheel velocity (rad/sec)>;R<right wheel position (rad)>,<right wheel velocity in rad/sec>;
      *
      * Serial.println() prints floating point values with 2 floating point
-     * digits; this is adequate because 0.01 radians is about a half degree.
+     * digits so we need to specify 8 sigfigs for more accuracy.
      */
-    const String wheel_vel_str = "L" + String(left_wheel_vel_rad_per_sec, 8) + ";R" + String(right_wheel_vel_rad_per_sec, 8) + ";";
+    const String wheel_vel_str = "L"  + String(left_wheel_pos_rads, 8) +
+                                 ","  + String(left_wheel_vel_rad_per_sec, 8) +
+                                 ";R" + String(right_wheel_pos_rads, 8) +
+                                 ","  + String(right_wheel_vel_rad_per_sec, 8) +
+                                 ";";
     Serial.println(wheel_vel_str);
 
-    // Reset encoder counts for next cycle
-    leftEncoder.write(0);
-    rightEncoder.write(0);
+    // Update previous encoder counts
+    prev_left_ticks_4x  = cur_left_ticks_4x;
+    prev_right_ticks_4x = cur_right_ticks_4x;
 
     // Update last time wheel velocities were computed
     last_time_ms = current_time_ms;
-  }
+  } // End time_delta_ms >= COMPUTE_TIME_MS
 }
