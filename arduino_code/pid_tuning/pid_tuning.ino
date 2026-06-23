d930e842472dc5f734efe85578fc25ab01b92574
Bryant Pong <rcxking@gmail.com>
2026-06-22 18:39:27 -0700

    Parsing next command

--- arduino_code/pid_tuning/pid_tuning.ino
+++ arduino_code/pid_tuning/pid_tuning.ino
@@ -109,7 +109,47 @@ void loop() {
      * specifier (L/R/P/I/D) and at least 1 character for the value).
      */
     if (nextCommand.length() >= 2) {
+      const char directive = nextCommand[0];
+      const int value = nextCommand.substring(1).toFloat();
 
+      if (directive == 'L') {
+        // Send desired left motor velocity
+        Serial.print("Writing to left motor rad/s: ");
+        Serial.println(value);
+        leftPID.SendPWMCommand(left_wheel_vel_rad_per_sec, value);
+      } else if (directive == 'R') {
+        // Send desired right motor velocity
+        Serial.print("Writing to right motor rad/s: ");
+        Serial.println(value);
+        rightPID.SendPWMCommand(right_wheel_vel_rad_per_sec, value);
+      } else if (directive == 'P') {
+        // Update proportional constants
+        Serial.print("Updating KP to: ");
+        Serial.println(value);
+        leftPID.SetKP(value);
+        rightPID.SetKP(value);
+        Serial.println("leftPID constants: " + leftPID.DisplayPIDConstants());
+        Serial.println("rightPID constants: " + rightPID.DisplayPIDConstants());
+      } else if (directive == 'I') {
+        // Update integral constants
+        Serial.print("Updating KI to: ");
+        Serial.println(value);
+        leftPID.SetKI(value);
+        rightPID.SetKI(value);
+        Serial.println("leftPID constants: " + leftPID.DisplayPIDConstants());
+        Serial.println("rightPID constants: " + rightPID.DisplayPIDConstants());
+      } else if (directive == 'D') {
+        // Update derivative constants
+        Serial.print("Updating KD to: ");
+        Serial.println(value);
+        leftPID.SetKD(value);
+        rightPID.SetKD(value);
+        Serial.println("leftPID constants: " + leftPID.DisplayPIDConstants());
+        Serial.println("rightPID constants: " + rightPID.DisplayPIDConstants());
+      } else {
+        // Unknown directive
+        Serial.println("ERROR: Unknown directive: " + nextCommand);
+      }
     } else {
       Serial.print("ERROR: Ignoring invalid command: ");
       Serial.println(nextCommand);
